ca1a0e1685833eba8c5f2e2f85d9d2278cbcc95f
hiropon-hiropon <76112392+hiropon-hiropon@users.noreply.github.com>
2021-01-10 01:27:12 +0900

    Add files via upload

---
+++ index.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta http-equiv="Content=Type" content="text/html; charset=utf-8">
+  <title>塩崎の試作ほむぺ</title>
+  <link rel="icon" href="favicon.ico">
+  <meta name="discription" content="塩崎のポートフォリオサイトです。">
+  <link rel="stylesheet" href="css/styles.css">
+</head>
+<body>
+  <header>
+    <div class="container">
+      <div class="icon">
+        <img src="img/taro.png" width="120" height="120" alt="塩崎のアイコン。">
+      </div>
+      <div class="info">
+        <h1>塩崎裕之</h1>
+        <p>只今 HTML CSS PHP 勉強中</p>
+        <ul>
+          <li>
+            <a href="https://retty.me/area/PRE13/ARE12/SUB1201/100001227782/" target="_blank">
+              <img src="img/blog.png" width="40" height="40" alt="グルメサイトへのリンク画像です">
+            </a>
+          </li>
+          <li>
+            <a href="https://www.facebook.com/suisenhigashi/" target="_blank">
+              <img src="img/photos.png" width="40" height="40" alt="Facebookへのリンク画像です">
+            </a>
+          </li>
+        </ul>
+      </div>
+      </div>
+  </header>
+  
+  <section class="works">
+    <h1>
+    以前やっていたお店の写真素材がMacにたくさん残っているので。<p>ドットインストールを見ながらその素材でホームページ風のものを作ってみました。</p>
+    </h1>
+
+    <section>
+      <img src="img/work1.png" width="400" height="260" alt="お店の外観">
+      <h1>粋鮮　東中野</h1>
+      <p>静岡県の沼津港から、直送で仕入れた鮮魚を売りにしている、いわゆる鮮魚推しの居酒屋ですね。</p>
+    </section>
+  
+    <section>
+      <img src="img/work2.png" width="400" height="260" alt="お刺身の画像">
+      <h1>お造り、いろいろ。</h1>
+      <p>鮮魚推しなので、やはりお刺身が人気でしたね。</p>
+    </section>
+    
+    <section>
+      <img src="img/work3.png" width="400" height="260" alt="その他の料理、日本酒の画像。">
+      <h1>その他の料理や日本酒。</h1>
+      <p>天ぷらや、煮魚、焼き魚、全国各地の地酒も。</p> 
+    </section>
+
+    <section>
+      <a href="bbs.php">掲示板</a>
+    </section>
+
+    <section>
+      <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3239.855061750351!2d139.68067341487395!3d35.70518413626599!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x6018f2c8f9d5bc87%3A0x6e1bfd39365bca32!2z44CSMTY0LTAwMDMg5p2x5Lqs6YO95Lit6YeO5Yy65p2x5Lit6YeO77yR5LiB55uu77yV77yZ4oiS77yUIOOCs-ODquODs-OCuu-8lO-8meODk-ODqw!5e0!3m2!1sja!2sjp!4v1610206584925!5m2!1sja!2sjp" width="400" height="300" frameborder="0" style="border:0;" allowfullscreen="" aria-hidden="false" tabindex="0"></iframe>
+    </section>
+
+  </section>
+
+  <footer>
+    <p>(c) Shiozaki Hiroyuki </p>
+  </footer>
+
+</body>
+</html>
+
